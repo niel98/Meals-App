aa0b027867b142b0898912bbcf0883a63c209df0
Niel <mosesdaniel201798@gmail.com>
2021-12-28 13:25:03 +0100

    Bottom navigation and icons added

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,12 +1,17 @@
+import React from 'react'
 import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
+import { createBottomTabNavigator } from 'react-navigation-tabs'
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs'
 import { Platform } from 'react-native'
 
 import CategoriesScreen from '../screens/CategoriesScreen'
 import CategoryMealScreen from '../screens/CategoryMealScreen'
 import MealDetailScreen from '../screens/MealDetailScreen'
+import FavoritesScreen from '../screens/FavoritesScreen'
 
 import Colors from '../constants/Colors'
+import { Ionicons } from '@expo/vector-icons'
 
 const MealsNavigator = createStackNavigator({
     Categories: CategoriesScreen,
@@ -22,4 +27,32 @@ const MealsNavigator = createStackNavigator({
     }
 })
 
-export default createAppContainer(MealsNavigator)
+const tabScreenConfig = {
+    Meals: { screen: MealsNavigator, navigationOptions: {
+        tabBarIcon: (tabInfo) => {
+            return <Ionicons name='ios-restaurant' size={25} color={tabInfo.tintColor}/>
+        },
+        tabBarColor: Colors.primaryColor
+    }},
+    Favorites: { screen: FavoritesScreen, navigationOptions: {
+        tabBarIcon: (tabInfo) => {
+            return <Ionicons name='ios-star' size={25} color={tabInfo.tintColor}/>
+        },
+        tabBarColor: Colors.accentColor
+    }}
+}
+
+const MealsFavNavigator = Platform.OS === 'android' ? createMaterialBottomTabNavigator(tabScreenConfig, {
+    activeColor: 'white',
+    shifting: true,
+    barStyle: {
+        backgroundColor: Colors.primaryColor
+    }
+}) : createBottomTabNavigator(tabScreenConfig,
+{
+    tabBarOptions: {
+        activeTintColor: Colors.accentColor
+    }
+})
+
+export default createAppContainer(MealsFavNavigator)
